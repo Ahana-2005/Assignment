103350a78f4122d1d52fdf2d55f210d990d73ebb
Ahana-2005 <ahana25052005@gmail.com>
2023-10-02 12:26:07 +0530

    create T.1

---
+++ T.1.cpp
@@ -0,0 +1,13 @@
+//Write a c program to print the pattern
+#include <stdio.h>
+int main()
+{
+    for(int i=1;i<=5;i++)
+    {
+        for(int j=1;j<=10;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
