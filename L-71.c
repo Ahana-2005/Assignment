e378ff642da752a2032c40f6e180035c8f0d454f
Ahana-2005 <ahana25052005@gmail.com>
2023-11-26 09:59:29 +0530

    Update L-71.c

--- L-71.c
+++ L-71.c
@@ -3,9 +3,9 @@
 int main()
 {
 int r,c;
-int matrix1[100][100];
-int matrix2[100][100];
-int result[100][100];
+int a[100][100];
+int b[100][100];
+int pro[100][100];
 int i, j, k;
 printf("Enter the number of rows: ");
 scanf("%d", &r);
@@ -17,7 +17,7 @@ for (i = 0; i < r; i++)
 for (j = 0; j < c; j++) 
 {
 printf("Enter the element in row-%d and column-%d :",i,j);
-scanf("%d", &matrix1[i][j]);
+scanf("%d", &a[i][j]);
 }
 }
 printf("Enter elements of the second 2x2 matrix:\n");
@@ -26,17 +26,17 @@ for (i = 0; i < r; i++)
 for (j = 0; j < c; j++) 
 {
 printf("Enter the element in row-%d and column-%d :",i,j);
-scanf("%d", &matrix2[i][j]);
+scanf("%d", &b[i][j]);
 }
 }
 for (i = 0; i < r; i++) 
 {
 for (j = 0; j < c; j++) 
 {
-result[i][j] = 0;
+pro[i][j] = 0;
 for (k = 0; k < 2; k++) 
 {
-result[i][j] += matrix1[i][k] * matrix2[k][j];
+pro[i][j] += a[i][k] * b[k][j];
 }
 }
 }
@@ -45,7 +45,7 @@ for (i = 0; i < r; i++)
 {
 for (j = 0; j < c; j++) 
 {
-printf("%d ", result[i][j]);
+printf("%d ", pro[i][j]);
 }
 printf("\n");
 }
